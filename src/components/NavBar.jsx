d55bdfbd02fc702ace2f2295e94c0b05b44ae676
Tushar Vashihst <tusharvashisht9905@gmail.com>
2025-04-12 19:02:38 +0530

    make login working

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router';
 
 const NavBar = () => {
 
@@ -8,7 +9,7 @@ const NavBar = () => {
   return (
     <div className="navbar bg-base-300 shadow-sm">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl">DevTinder</a>
+          <Link to="/" className="btn btn-ghost text-xl">DevTinder</Link>
         </div>
         {user && (<div className="flex gap-2">
          <div className="dropdown dropdown-end mx-5 flex">
@@ -31,10 +32,10 @@ const NavBar = () => {
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
               <li>
-                <a className="justify-between">
+                <Link to="/profile" className="justify-between">
                   Profile
                   <span className="badge">New</span>
-                </a>
+                </Link>
               </li>
               <li>
                 <a>Settings</a>
